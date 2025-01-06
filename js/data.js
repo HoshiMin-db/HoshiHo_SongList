e34017a1b5252462948d9a7ed4a9d6b7457d01db
HoshiMin-db <sasuke0323jp@gmail.com>
2025-01-07 05:04:03 +0900

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,37 @@
+let allData = [];
+let totalSongCount = 0;
+
+function fetchData(callback) {
+    fetch('data.json', { cache: 'no-cache' })
+        .then(response => response.json())
+        .then(data => {
+            allData = data;
+            if (totalSongCount === 0) {
+                const uniqueSongs = new Set(data.map(item => `${normalizeString(item.song_name)}-${normalizeString(item.artist)}`));
+                totalSongCount = uniqueSongs.size;
+                document.getElementById('songCount').textContent = totalSongCount;
+            }
+            callback();
+        });
+}
+
+function fetchAndDisplayData(query, numDates = 3) {
+    songTableBody.innerHTML = '';
+
+    const filteredData = allData.filter(row =>
+        normalizeString(row.song_name).toLowerCase().includes(query) ||
+        normalizeString(row.artist).toLowerCase().includes(query) ||
+        normalizeString(row.source).toLowerCase().includes(query)
+    );
+
+    const replaceSongs = {
+        'rorikami': '粛聖‼ ロリ神レクイエム☆'
+    };
+    filteredData.forEach(row => {
+        if (replaceSongs[row.song_name]) {
+            row.song_name = replaceSongs[row.song_name];
+        }
+    });
+
+    displayData(filteredData, numDates);
+}
