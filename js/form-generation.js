c74c8debb17c577661e25077d8f84e6c8bb7e87d
HoshiMin-db <sasuke0323jp@gmail.com>
2025-01-07 05:14:30 +0900

    Update form-generation.js

--- js/form-generation.js
+++ js/form-generation.js
@@ -1,6 +1,5 @@
-// js/form-generation.js
 import './events.js';
-import { fetchData } from './data.js';
+import { fetchData, fetchAndDisplayData } from './data.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     fetchData(() => fetchAndDisplayData(''));
