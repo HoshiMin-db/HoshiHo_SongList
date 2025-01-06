327cf324c289dac3072badf8923770c6b40c7035
HoshiMin-db <sasuke0323jp@gmail.com>
2025-01-07 05:06:27 +0900

    Update event.js

--- js/event.js
+++ js/event.js
@@ -1,3 +1,7 @@
+// js/events.js
+import { debounce, normalizeString } from './utils.js';
+import { fetchAndDisplayData } from './data.js';
+
 document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById('searchInput');
     const showAllButton = document.getElementById('showAllButton');
@@ -8,15 +12,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }, 300));
 
     showAllButton.addEventListener('click', function() {
-        showAllState = !showAllState;
+        const showAllState = !showAllButton.classList.contains('button-on');
         showAllButton.classList.toggle('button-on', showAllState);
         showAllButton.classList.toggle('button-off', !showAllState);
         showAllButton.textContent = showAllState ? "隱藏" : "顯示全部";
 
-        if (showAllState) {
-            displayData(allData, allData.length); // 顯示所有日期
-        } else {
-            fetchAndDisplayData(''); // 顯示最近3個日期
-        }
+        fetchAndDisplayData('', showAllState ? Infinity : 3);
     });
 });
