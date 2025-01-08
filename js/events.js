d6e071e431b6a1361f3a4e5cfb5ed84efaf3dc74
HoshiMin-db <sasuke0323jp@gmail.com>
2025-01-09 04:44:34 +0900

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,5 +1,6 @@
 // events.js
-import { debounce, normalizeString, fetchAndDisplayData, fetchData, allData } from './data.js';
+import { debounce, normalizeString } from './utils.js';
+import { fetchAndDisplayData, fetchData, allData } from './data.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     const searchInput = document.getElementById('searchInput');
@@ -39,41 +40,6 @@ function onScroll() {
     tbody.innerHTML = '';
     
     // 渲染可視區域內的數據
-    for (let i = startIdx; i < endIdx; i++) {
-        const row = allData[i];
-        const newRow = tbody.insertRow();
-        newRow.insertCell().textContent = row.song_name.charAt(0).toUpperCase();
-        newRow.insertCell().textContent = row.song_name;
-        newRow.insertCell().textContent = row.artist;
-        newRow.insertCell().textContent = row.source;
-
-        // 檢查 row.dates 是否存在且為陣列
-        if (Array.isArray(row.dates)) {
-            row.dates.slice(0, 3).forEach(date => {
-                const dateCell = newRow.insertCell();
-                dateCell.classList.add('date-cell');
-                const link = document.createElement('a');
-                link.href = date.link;
-                link.textContent = date.formattedDate;
-                link.target = '_blank';
-                link.onclick = function(event) {
-                    event.preventDefault();
-                    openFloatingPlayer(link.href);
-                };
-                dateCell.appendChild(link);
-
-                if (date.is_member_exclusive) {
-                    const lockIcon = document.createElement('span');
-                    lockIcon.classList.add('lock-icon');
-                    lockIcon.textContent = '🔒';
-                    link.appendChild(lockIcon);
-                }
-                if (date.is_acapella) {
-                    dateCell.classList.add('acapella');
-                }
-            });
-        } else {
-            console.error(`Dates property is missing or not an array for row:`, row);
-        }
-    }
+    const visibleData = allData.slice(startIdx, endIdx);
+    displayData(visibleData, 3);
 }
